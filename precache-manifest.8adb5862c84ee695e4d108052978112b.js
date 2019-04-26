71f5162fd0453a7943ea6a3c99cd846db52e37f8
Rose McClain <rosemcclain024@gmail.com>
2019-04-25 23:00:05 -0400

    Updates

---
+++ precache-manifest.8adb5862c84ee695e4d108052978112b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd54e8306819e918008c2b848640f50a",
+    "url": "/rosemcclain024/clickygame/index.html"
+  },
+  {
+    "revision": "52264075e3ff0abde307",
+    "url": "/rosemcclain024/clickygame/static/css/main.bd8d38ef.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/rosemcclain024/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "52264075e3ff0abde307",
+    "url": "/rosemcclain024/clickygame/static/js/main.9efff5ea.chunk.js"
+  },
+  {
+    "revision": "0dd2b913e6e53d323745",
+    "url": "/rosemcclain024/clickygame/static/js/runtime~main.95942ec4.js"
+  }
+]);
